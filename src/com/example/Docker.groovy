70779bf62965e24a20b96353f574d71beb566598
Raju George <raju.george@yahoo.com>
2021-10-21 16:44:59 +0530

    splitted docker functionalities

--- src/com/example/Docker.groovy
+++ src/com/example/Docker.groovy
@@ -11,11 +11,18 @@ class Docker implements Serializable {
 
     def buildDockerImage(String imageName){
         script.echo 'building the image....'
+        script.sh "docker build -t $imageName ."
+    }
+
+    def dockerLogin(){
         script.withCredentials([script.usernamePassword(credentialsId: 'nexus-login-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]){
-            script.sh "docker build -t $imageName ."
             script.sh 'echo $PASS | docker login -u $USER --password-stdin 64.227.176.229:8083'
             script.sh "docker push $imageName"
         }
+    }
+
+    def dockerPush(String imageName){
+        script.sh "docker push $imageName"
     }
 
 }
