1e11c4eb66b206352f7d3832cc875d496a6557d1
Raju George <raju.george@yahoo.com>
2021-10-21 15:59:04 +0530

    parameterized

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,10 +1,10 @@
 #!/usr/bin/env groovy
 
-def call(){
+def call(String imageName){
     echo 'building the image....'
     withCredentials([usernamePassword(credentialsId: 'nexus-login-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]){
-        sh 'docker build -t 64.227.176.229:8083/movies-app:1.3 .'
+        sh "docker build -t $imageName ."
         sh 'echo $PASS | docker login -u $USER --password-stdin 64.227.176.229:8083'
-        sh 'docker push 64.227.176.229:8083/movies-app:1.3'
+        sh "docker push $imageName"
     }
 }
