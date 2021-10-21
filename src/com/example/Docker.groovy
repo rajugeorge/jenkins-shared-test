4568569e498b321b143b5a351cb0645ef262a198
Raju George <raju.george@yahoo.com>
2021-10-21 16:21:01 +0530

    incorporated docker class

---
+++ src/com/example/Docker.groovy
@@ -0,0 +1,20 @@
+#!/usr/bin/env groovy
+
+class Docker implements Serializable {
+
+    def script 
+
+    Docker(script) {
+        this.script = script
+    }
+
+    def buildDockerImage(String imageName){
+        script.echo 'building the image....'
+        script.withCredentials([usernamePassword(credentialsId: 'nexus-login-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]){
+            script.sh "docker build -t $imageName ."
+            script.sh 'echo $script.PASS | docker login -u $script.USER --password-stdin 64.227.176.229:8083'
+            script.sh "docker push $imageName"
+        }
+    }
+
+}
--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,10 +1,7 @@
 #!/usr/bin/env groovy
 
+import Docker
+
 def call(String imageName){
-    echo 'building the image....'
-    withCredentials([usernamePassword(credentialsId: 'nexus-login-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]){
-        sh "docker build -t $imageName ."
-        sh 'echo $PASS | docker login -u $USER --password-stdin 64.227.176.229:8083'
-        sh "docker push $imageName"
-    }
+    return new Docker(this).buildDockerImage(imageName)
 }
