1804952fdadeb28ec40c6ffce8d2675f119f9be7
Raju George <raju.george@yahoo.com>
2021-10-21 15:50:01 +0530

    added build image groovy script

---
+++ vars/buildImage.groovy
@@ -0,0 +1,10 @@
+#!/usr/bin/env groovy
+
+def call(){
+    echo 'building the image....'
+    withCredentials([usernamePassword(credentialsId: 'nexus-login-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]){
+        sh 'docker build -t 64.227.176.229:8083/movies-app:1.3 .'
+        sh 'echo $PASS | docker login -u $USER --password-stdin 64.227.176.229:8083'
+        sh 'docker push 64.227.176.229:8083/movies-app:1.3'
+    }
+}
