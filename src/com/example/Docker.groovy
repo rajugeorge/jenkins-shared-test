5fcb795aef2dc5589f88ee17e79f6e02c7b12922
Raju George <raju.george@yahoo.com>
2021-10-21 16:28:55 +0530

    script error fixed

--- src/com/example/Docker.groovy
+++ src/com/example/Docker.groovy
@@ -11,9 +11,9 @@ class Docker implements Serializable {
 
     def buildDockerImage(String imageName){
         script.echo 'building the image....'
-        script.withCredentials([usernamePassword(credentialsId: 'nexus-login-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]){
+        script.withCredentials([script.usernamePassword(credentialsId: 'nexus-login-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]){
             script.sh "docker build -t $imageName ."
-            script.sh 'echo $script.PASS | docker login -u $script.USER --password-stdin 64.227.176.229:8083'
+            script.sh "echo $script.PASS | docker login -u $script.USER --password-stdin 64.227.176.229:8083"
             script.sh "docker push $imageName"
         }
     }
